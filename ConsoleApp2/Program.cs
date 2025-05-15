f863fb2df2d4c5e7299435b6e77895bee179a35c
aptem1223 <sanstop357@gmail.com>
2025-05-15 10:29:21 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static double Min(double a, double b)
+        {
+            return a < b ? a : b;
+        }
+
+        public static double Max(double a, double b)
+        {
+            return a > b ? a : b;
+        }
+
+        public delegate double CompareDelegate(double x, double y);
+
+        static void Main(string[] args)
+        {
+            CompareDelegate minDelegate = Min;
+            CompareDelegate maxDelegate = Max;
+
+            double num1 = 5.4;
+            double num2 = 8.2;
+
+            double minResult = minDelegate(num1, num2);
+            double maxResult = maxDelegate(num1, num2);
+
+            Console.WriteLine($"Минимальное: {minResult}");
+            Console.WriteLine($"Максимальное: {maxResult}");
+            Console.Read();
+        }
+    }
+
+}
